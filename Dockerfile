1aa5289f4c49f9d908acc04e1f992df0dc93d54c
ingestbot <ingestbot>
2024-10-12 13:36:05 -0700

    Commiting all revisions from noisier_alt, developed ci/cd pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,14 @@
-FROM python:3.9.13-alpine
+FROM python:alpine
+RUN adduser -D -H noisier
+RUN mkdir /app && chown noisier:noisier /app
+USER noisier
 WORKDIR /app
+HEALTHCHECK --interval=30s --timeout=5s --retries=3 CMD "ps uxww | grep -v grep | grep noisier" || exit 1
+RUN python -m venv /app/venv
+ENV PATH="/app/venv/bin:${PATH}"
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt --user
-WORKDIR /app
-COPY . .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY noisier.py .
+COPY config.json . 
 ENTRYPOINT [ "python", "/app/noisier.py", "--config", "/app/config.json"]
+
--- noisier.py
+++ noisier.py
@@ -1,35 +1,50 @@
-
-import socket
 import argparse
 import datetime
 import json
+import logging
 import random
 import re
-import sys
+import socket
 import time
-import requests
-from requests.exceptions import SSLError, RequestException, ReadTimeout
 from urllib.parse import urljoin, urlparse
 
-from urllib3.exceptions import LocationParseError
+# import functools
+import requests
+import requests.exceptions
 
-##
-## See Crawler._request():
-##  - https://stackoverflow.com/questions/23013220/max-retries-exceeded-with-url-in-requests
-##
+#
+# https://prometheus.github.io/client_python/
+#
+# from prometheus_client import start_http_server, Summary, Counter
+from prometheus_client import Counter, start_http_server
+
+#
+# See Crawler._request(): https://stackoverflow.com/q/23013220
+#
 from requests.adapters import HTTPAdapter
+from urllib3.exceptions import LocationParseError
 from urllib3.util.retry import Retry
 
-import logging
-
-##
-## This prevents WARNING messages from urllib3 appearing in 
-## default INFO logging. 
-##
+#
+# This prevents WARNING messages from urllib3 appearing in
+# default INFO logging.
+#
 logging.getLogger("urllib3").setLevel(logging.ERROR)
+
+"""
+def time_it(metric):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with metric.time():
+                return func(*args, **kwargs)
+        return wrapper
+    return decorator
+"""
 
 
 class Crawler(object):
+
     def __init__(self):
         """
         Initializes the Crawl class
@@ -40,12 +55,30 @@ class Crawler(object):
         self.count_visit = 0
         self.count_error = 0
         self.count_bad_url = 0
-        self.kbytes_transferred = 0 
+        self.kbytes_transferred = 0
+
+        # Prometheus Metrics
+        # self.request_time = Summary('crawler_request_processing_seconds', 'Time spent processing requests')
+        self.prom_count_visit = Counter(
+            "crawler_count_visit", "Total visits made by the crawler"
+        )
+        self.prom_count_error = Counter(
+            "crawler_count_error", "Total errors encountered by the crawler"
+        )
+        self.prom_count_bad_url = Counter(
+            "crawler_bad_url", "Total bad URLs encountered by the crawler"
+        )
+        self.prom_kbytes_transferred = Counter(
+            "crawler_kbytes_transferred", "Total Kilobytes transferred by the crawler"
+        )
+
+        # self.crawl = time_it(self.request_time)(self.crawl)
 
     class CrawlerTimedOut(Exception):
         """
         Raised when the specified timeout is exceeded
         """
+
         pass
 
     def _request(self, url):
@@ -55,27 +88,29 @@ class Crawler(object):
         :return: the response Requests object
         """
         random_user_agent = random.choice(self._config["user_agents"])
-        headers = {'user-agent': random_user_agent}
+        headers = {"user-agent": random_user_agent}
 
-        session = requests.Session() 
+        session = requests.Session()
 
-        retry = Retry(total=3, 
-                    backoff_factor=1,
-                    status_forcelist=[500, 502, 503, 504],
-                    allowed_methods=["HEAD", "GET", "OPTIONS", "POST"]
-                    )
+        retry = Retry(
+            total=3,
+            backoff_factor=1,
+            status_forcelist=[500, 502, 503, 504],
+            allowed_methods=["HEAD", "GET", "OPTIONS", "POST"],
+        )
 
         adapter = HTTPAdapter(max_retries=retry)
-        session.mount('http://', adapter) 
-        session.mount('https://', adapter) 
+        session.mount("http://", adapter)
+        session.mount("https://", adapter)
 
         try:
             response = session.get(url, headers=headers, timeout=3)
             response.raise_for_status()
 
-            content_length = response.headers.get('Content-Length')
+            content_length = response.headers.get("Content-Length")
             if content_length is not None:
                 self.kbytes_transferred += int(content_length) / 1024
+                self.prom_kbytes_transferred.inc(int(content_length) / 1024)
 
             return response
 
@@ -84,27 +119,31 @@ class Crawler(object):
             if response.status_code == 503:
                 logging.debug(f"503 Service Unavailable for URL {url}.")
             self.count_error += 1
+            self.prom_count_error.inc()
 
         except requests.exceptions.ReadTimeout:
             logging.debug(f"Read timeout for URL {url}. Skipping this URL.")
             self.count_error += 1
+            self.prom_count_error.inc()
 
         except requests.exceptions.SSLError as e:
             logging.debug(f"SSL error for URL {url}: {e}")
             self.count_error += 1
+            self.prom_count_error.inc()
 
         except requests.exceptions.RequestException as e:
             logging.debug(f"_request(): Request error for URL {url}: {e}")
             self.count_error += 1
+            self.prom_count_error.inc()
 
         return None  # Return None if there's an error
 
     @staticmethod
     def _normalize_link(link, root_url):
         """
-        Normalizes links extracted from the DOM by making them all absolute, so
-        we can request them, for example, turns a "/images" link extracted from https://imgur.com
-        to "https://imgur.com/images"
+        Normalizes links extracted from the DOM by making them all absolute,
+        so we can request them, for example, turns a "/images" link extracted
+        from https://imgur.com to "https://imgur.com/images"
         :param link: link found in the DOM
         :param root_url: the URL the DOM was loaded from
         :return: absolute link
@@ -119,7 +158,7 @@ class Crawler(object):
 
         # '//' means keep the current protocol used to access this URL
         if link.startswith("//"):
-            return "{}://{}{}".format(parsed_root_url.scheme, parsed_url.netloc, parsed_url.path)
+            return f"{parsed_root_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
 
         # possibly a relative path
         if not parsed_url.scheme:
@@ -131,18 +170,20 @@ class Crawler(object):
     def _is_valid_url(url):
         """
         Check if a url is a valid url.
-        Used to filter out invalid values that were found in the "href" attribute,
-        for example "javascript:void(0)"
+        Used to filter out invalid values that were found in the "href"
+        attribute, for example "javascript:void(0)"
         taken from https://stackoverflow.com/questions/7160737
         :param url: url to be checked
         :return: boolean indicating whether the URL is valid or not
         """
         regex = re.compile(
-            r'^(?:http|ftp)s?://'  # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-            r'(?::\d+)?'  # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+            r"^(?:http|ftp)s?://"  # http:// or https://
+            r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
+            r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
+            r"(?::\d+)?"  # optional port
+            r"(?:/?|[/?]\S+)$",
+            re.IGNORECASE,
+        )
         return re.match(regex, url) is not None
 
     def _is_blacklisted(self, url):
@@ -151,13 +192,18 @@ class Crawler(object):
         :param url: full URL
         :return: boolean indicating whether a URL is blacklisted or not
         """
-        return any(blacklisted_url in url for blacklisted_url in self._config["blacklisted_urls"])
+        return any(
+            blacklisted_url in url
+            for blacklisted_url in self._config["blacklisted_urls"]
+        )
 
     def _should_accept_url(self, url):
         """
-        filters url if it is blacklisted or not valid, we put filtering logic here
+        filters url if it is blacklisted or not valid, we put filtering logic
+        here:
         :param url: full url to be checked
-        :return: boolean of whether or not the url should be accepted and potentially visited
+        :return: boolean of whether or not the url should be accepted and
+         potentially visited
         """
         return url and self._is_valid_url(url) and not self._is_blacklisted(url)
 
@@ -183,7 +229,7 @@ class Crawler(object):
         and blacklists it so we don't visit it in the future
         :param link: link to remove and blacklist
         """
-        self._config['blacklisted_urls'].append(link)
+        self._config["blacklisted_urls"].append(link)
 
         if link not in self._links:
             self._links.append(link)
@@ -193,13 +239,13 @@ class Crawler(object):
     def _browse_from_links(self, depth=0):
         """
         Selects a random link out of the available link list and visits it.
-        Blacklists any link that is not responsive or that contains no other links.
-        Please note that this function is recursive and will keep calling itself until
-        a dead end has reached or when we ran out of links
+        Blacklists any link that is not responsive or that contains no other
+        links. Please note that this function is recursive and will keep calling
+        itself until a dead end has reached or when we ran out of links
         :param depth: our current link depth
         """
 
-        is_depth_reached = depth >= self._config['max_depth']
+        is_depth_reached = depth >= self._config["max_depth"]
 
         if not len(self._links) or is_depth_reached:
             logging.debug("Hit a dead end, moving to the next root URL")
@@ -214,6 +260,7 @@ class Crawler(object):
 
         try:
             ip_address = socket.gethostbyname(fqdn)
+            logging.debug(f"Resolved IP Address: {ip_address}")
         except socket.gaierror:
             self._remove_and_blacklist(random_link)
 
@@ -223,13 +270,15 @@ class Crawler(object):
             logging.debug(f"Response: {response}")
 
             if response is None:
-                logging.debug(f"Skipping {random_link} due to request failure.")
+                logging.debug(f"Skipping {random_link} due to " "request failure.")
                 self.count_bad_url += 1
+                self.prom_count_bad_url.inc()
 
                 return
 
-            sub_page = response.content  # Access .content only if response is not None
+            sub_page = response.content
 
+            self.prom_count_visit.inc()
             self.count_visit += 1
 
             sub_links = self._extract_urls(sub_page, random_link)
@@ -240,8 +289,9 @@ class Crawler(object):
                 logging.info(f"Invalid URLs: {self.count_bad_url}")
                 logging.info(f"Total KBytes Transferred: {self.kbytes_transferred:.2f}")
 
-            # sleep for a random amount of time
-            time.sleep(random.randrange(self._config["min_sleep"], self._config["max_sleep"]))
+            time.sleep(
+                random.randrange(self._config["min_sleep"], self._config["max_sleep"])
+            )
 
             # make sure we have more than 1 link to pick from
             if len(sub_links) > 1:
@@ -252,20 +302,24 @@ class Crawler(object):
                 self._remove_and_blacklist(random_link)
 
         except requests.exceptions.RequestException:
-            logging.debug("Exception on URL: %s, removing from list and trying again!" % random_link)
+            logging.debug(
+                "Exception on URL: %s, removing from list and trying again!"
+                % random_link
+            )
             self._remove_and_blacklist(random_link)
             self.count_error += 1
+            self.prom_count_error.inc()
 
         self._browse_from_links(depth + 1)
 
     def load_config_file(self, file_path):
         """
-        Loads and decodes a JSON config file, sets the config of the crawler instance
-        to the loaded one
+        Loads and decodes a JSON config file, sets the config of the crawler
+        instance to the loaded one
         :param file_path: path of the config file
         :return:
         """
-        with open(file_path, 'r') as config_file:
+        with open(file_path, "r") as config_file:
             config = json.load(config_file)
             self.set_config(config)
 
@@ -296,8 +350,11 @@ class Crawler(object):
         is specified then return false
         :return: boolean indicating whether the timeout has reached
         """
-        is_timeout_set = self._config["timeout"] is not False  # False is set when no timeout is desired
-        end_time = self._start_time + datetime.timedelta(seconds=self._config["timeout"])
+        # False is set when no timeout is desired
+        is_timeout_set = self._config["timeout"] is not False
+        end_time = self._start_time + datetime.timedelta(
+            seconds=self._config["timeout"]
+        )
         is_timed_out = datetime.datetime.now() >= end_time
 
         return is_timeout_set and is_timed_out
@@ -307,7 +364,6 @@ class Crawler(object):
         Collects links from root_urls, stores them, then calls
         `_browse_from_links` to browse.
         """
-
         self._start_time = datetime.datetime.now()
         logging.info(f"Time is now: {self._start_time}")
 
@@ -317,68 +373,93 @@ class Crawler(object):
             url = random.choice(self._config["root_urls"])
             fqdn = urlparse(url).netloc
 
+            if len(self._links) == 0:
+                logging.debug(f"Priming with: {url}")
             if url in bad_urls:
                 continue
             try:
                 ip_address = socket.gethostbyname(fqdn)
+                logging.debug(f"Resolved IP Address: {ip_address}")
             except socket.gaierror:
                 bad_urls.add(url)
                 self.count_bad_url += 1
+                self.prom_count_bad_url.inc()
                 continue
 
             try:
-                response = self._request(url) 
+                response = self._request(url)
                 if response is None:
                     bad_urls.add(url)
                     self.count_bad_url += 1
+                    self.prom_count_bad_url.inc()
                     continue
                 body = response.content
 
                 self._links = self._extract_urls(body, url)
-                logging.debug("found {} links".format(len(self._links)))
+                logging.debug(f"URL is good: {url}. Found {len(self._links)} links.")
 
                 self._browse_from_links()
 
             except requests.exceptions.RequestException:
                 logging.warning("Error connecting to root url: {}".format(url))
                 self.count_error += 1
-                
+                self.prom_count_error.inc()
+
             except MemoryError:
-                logging.warning("Error: content at url: {} is exhausting the memory".format(url))
+                logging.warning(
+                    "Error: content at url: {} is exhausting the memory".format(url)
+                )
                 self.count_error += 1
+                self.prom_count_error.inc()
 
             except LocationParseError:
                 logging.warning("Error encountered during parsing of: {}".format(url))
                 self.count_error += 1
+                self.prom_count_error.inc()
 
             except self.CrawlerTimedOut:
                 logging.info("Timeout has exceeded, exiting")
                 return
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log', metavar='-l', type=str, help='logging level', default='info')
-    parser.add_argument('--config', metavar='-c', required=True, type=str, help='config file')
-    parser.add_argument('--timeout', metavar='-t', required=False, type=int,
-                        help='for how long the crawler should be running, in seconds', default=False)
+    parser.add_argument(
+        "--log", metavar="-l", type=str, help="logging level", default="info"
+    )
+    parser.add_argument(
+        "--config", metavar="-c", required=True, type=str, help="config file"
+    )
+    parser.add_argument(
+        "--timeout",
+        metavar="-t",
+        required=False,
+        type=int,
+        help="for how long the crawler should be running, in seconds",
+        default=False,
+    )
     args = parser.parse_args()
 
     level = getattr(logging, args.log.upper(), logging.INFO)
 
     logging.basicConfig(
-      format='%(asctime)s %(levelname)-8s %(message)s',
-      level=level,
-      datefmt='%Y-%m-%d %H:%M:%S')
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=level,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    start_http_server(8000)
 
     crawler = Crawler()
     crawler.load_config_file(args.config)
 
     if args.timeout:
-        crawler.set_option('timeout', args.timeout)
+        crawler.set_option("timeout", args.timeout)
 
     logging.info("Starting Noisier!")
 
     crawler.crawl()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
